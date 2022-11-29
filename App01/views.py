5d44e5c61d55212671825f31f9aeb97361dd9fa7
JioJio <jiojaxson@gmail.com>
2022-11-29 17:25:37 +0800

    'django模板继承'

--- App01/views.py
+++ App01/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 
+ # 类似router
 # Create your views here.
 def index(request):
-    img_list = [
-        "http://127.0.0.1:8000/media/site_bg/31.jpg",
-        "http://127.0.0.1:8000/media/site_bg/29.jpg",
-    ]
+    return render(request, 'index.html')
 
-    return render(request,'index.html',{'img_list':img_list})
+
+def news(request):
+    return render(request, 'news.html')
