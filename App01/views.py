1d28a5438fedf1a1e0c223093273ebdc82515b42
JioJio <jiojaxson@gmail.com>
2022-11-29 12:25:04 +0800

    'django列表遍历'

--- App01/views.py
+++ App01/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    img_list = [
+        "../media/site_bg/31.jpg",
+        "../media/site_bg/29.jpg",
+        "../media/site_bg/28.jpg",
+    ]
+
+    return render(request,'index.html',{'img_list':img_list})
