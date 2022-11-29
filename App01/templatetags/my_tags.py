e374325f69b925be32c489a3bb4a7800804de969
JioJio <jiojaxson@gmail.com>
2022-11-29 14:15:46 +0800

    'inclusion_tag自定义返回页面'

--- App01/templatetags/my_tags.py
+++ App01/templatetags/my_tags.py
@@ -4,6 +4,15 @@ register = template.Library()
 
 
 # 自定义过滤器
-@register.filter
-def add2(item):
-    return int(item) + 2
+# @register.filter
+# def add2(item):
+#     return int(item) + 2
+
+@register.inclusion_tag('my_tags/headers.html')
+def banner(menu_name):
+    print(menu_name)
+    img_list = [
+        "http://127.0.0.1:8000/media/site_bg/31.jpg",
+        "http://127.0.0.1:8000/media/site_bg/29.jpg",
+    ]
+    return {'img_list': img_list}
--- App01/views.py
+++ App01/views.py
@@ -3,9 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     img_list = [
-        "../media/site_bg/31.jpg",
-        "../media/site_bg/29.jpg",
-        "../media/site_bg/28.jpg",
+        "http://127.0.0.1:8000/media/site_bg/31.jpg",
+        "http://127.0.0.1:8000/media/site_bg/29.jpg",
     ]
 
     return render(request,'index.html',{'img_list':img_list})
