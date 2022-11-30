dc977a8e48a433f966f8b43d2987249bf6099e91
JioJio <jiojaxson@gmail.com>
2022-11-30 23:03:36 +0800

    'django中间件处理优化'

--- App01/middleware_decode.py
+++ App01/middleware_decode.py
@@ -6,9 +6,8 @@ import json
 class Md1(MiddlewareMixin):
     # 请求中间件
     def process_request(self, request):
-        if request.method == 'POST':
+        if request.method == 'POST' and request.META.get('CONTENT_TYPE') == 'application/json':
             data = json.loads(request.body)
-            print(data)
             request.data = data
 
     def process_response(self, request, response):
