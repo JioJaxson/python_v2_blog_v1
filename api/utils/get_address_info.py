8616cdb093d01376488745f61026d34e49ad601d
JioJaxson <jiojaxson@gmail.com>
2022-12-22 10:56:47 +0800

    '爬虫_数据解析获取城市名'

--- api/utils/get_address_info.py
+++ api/utils/get_address_info.py
@@ -1,4 +1,5 @@
 import requests
+import re
 # 乱码问题
 # 1: charset="gb2312"时
 # 先转二进制(.content) 再解码(decode('gbk'))
@@ -7,8 +8,11 @@ headers = {
 }
 def get_address_info(ip):
     url = f'https://www.ip138.com/iplookup.asp?ip={ip}&action=2'
-    res = requests.get(url=url, headers=headers).content
-    print(res.decode('gbk'))
+    res = requests.get(url=url, headers=headers).content.decode('gbk')
+    result = re.findall(r'ip_result = (.*?);', res, re.S)[0] #正则获取数据为数组,将数组转为对象
+    consequence = eval(result) #转成字典json格式
+    address = consequence['ip_c_list']
+    print(address)
 
 
 if __name__ == '__main__':
